65605bb947229c6a547fdf66c996c6d096268636
chhote12 <singhchhote695@gmail.com>
2025-07-07 20:59:22 +0530

    Your commit message

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connetionfunction=async()=>{
  try{
-          await mongoose.connect("mongodb://localhost:27017/fooddbgreatstack");
+          await mongoose.connect(process.env.MONGO_URL);
           console.log("success")
  }
  catch(err){
